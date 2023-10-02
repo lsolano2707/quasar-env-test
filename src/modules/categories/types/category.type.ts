40d1f4b16114b82290ea2107827c43c819caf5d8
Leonardo Solano <lsolano4@gmail.com>
2023-10-01 20:53:59 -0500

    test 2

--- src/modules/categories/types/category.type.ts
+++
@@ -1,8 +0,0 @@
-export interface Category {
-  id: number;
-  name: string;
-  image: string;
-}
-//Customizar: (cuando necesito alguna propiedad especifica del objeto).
-export type CreateCategoryDTO = Required<Pick<Category, 'name' | 'image'>>;
-export type UpdateCategoryDTO = Partial<CreateCategoryDTO>;
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,27 +1,6 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <h1>ENV TEST - QUASAR 1</h1>
-    <!-- <div class="col-auto q-mr-sm">
-      <q-btn
-        color="primary"
-        rounded
-        @click="goToProductsPage"
-        :label="$t('buttons.products')"
-      />
-    </div>
-    <div class="col-auto q-mr-sm">
-      <q-btn
-        color="primary"
-        rounded
-        @click="goToCategoryPage"
-        :label="$t('buttons.categories')"
-      />
-    </div> -->
-    <!-- environment:: {{ process.env.ENVIRONMENT }} -->
-    <br>
-    environment 1:: {{ environment }}
-    <br>
-    environment 2:: {{ environment2 }}
+    <h1>ENV TEST - QUASAR 2</h1>
   </q-page>
 </template>
 
