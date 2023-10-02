e4cb7f6c4452910f1b56544c66407f707c773d3d
Leonardo Solano <lsolano4@gmail.com>
2023-10-01 22:56:20 -0500

    test

--- src/libs/country-state-city/helpers/file.helper.ts
+++ src/libs/country-state-city/helpers/file.helper.ts
@@ -1,5 +1,9 @@
 export async function readJsonFile(path: string) {
+  console.log('path', path);
   const file = await import(path);
+  console.log('file', file);
+  console.log('file.default', file.default);
+
   return file.default;
 }
 
