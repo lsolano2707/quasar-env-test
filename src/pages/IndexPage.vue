3b2b3656636fed6be500dcc55437ace8548aca68
Leonardo Solano <lsolano4@gmail.com>
2023-10-01 19:57:45 -0500

    test

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -26,20 +26,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-
-// Composables
-import { useProduct } from 'src/modules/products/composables/useProducts';
-import { useCategories } from 'src/modules/categories/composables/useCategories';
-
-// Constants
-import { ENVIRONMENT } from 'src/modules/common/constants/env.constant';
-
-// Variables
-const { goToHomePage: goToProductsPage } = useProduct();
-const { goToHomePage: goToCategoryPage } = useCategories();
-const environment = ref(ENVIRONMENT);
-const environment2 = ref(process.env.ENVIRONMENT);
+import { onMounted } from 'vue';
 
 onMounted(() => {
   console.log('ENVIRONMENT', process.env.ENVIRONMENT);
